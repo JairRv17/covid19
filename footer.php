438996d9228304ab08a7673a58e06611fe32c733
JairRv17 <jair.roca@cu.ucsg.edu.ec>
2021-02-21 15:04:35 -0500

    Añadiendo header y footer

---
+++ footer.php
@@ -0,0 +1,17 @@
+</div>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<!--<script src="https://kit.fontawesome.com/9d1949eb9a.js" crossorigin="anonymous"></script>-->
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"> </script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+include_once('conexion.php');
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="font-awesome/css/fontawesome.min.css">
+    <link href="style/styles.css" rel="stylesheet">-->
+
+    
+    <!-- Custom styles for this template -->
+    <link href="styles/styles.css" rel="stylesheet">
+
+    <title><?php echo $title; ?></title>
+
+</head>
+
+<body>
+    <div class="container">
